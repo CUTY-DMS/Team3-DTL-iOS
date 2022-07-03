cabdcc7b22dbb81117f92661fa2d169b55b370a6
InhyeKang <kanginhae0826@gmail.com>
2022-07-03 23:17:06 +0900

    [ADD] FIX UI

---
+++ TodoList/TodoList/TabBarVC/MainVC.swift
@@ -0,0 +1,33 @@
+//
+//  MainVC.swift
+//  TodoList
+//
+//  Created by 강인혜 on 2022/07/03.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    @IBOutlet weak var listTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnCheckBox(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
