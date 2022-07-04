00ef544a0dca7e29b26f8cd59338d98ad4c3feac
InhyeKang <kanginhae0826@gmail.com>
2022-07-04 11:51:27 +0900

    🌧 :: (#2) 체크박스 버튼 생성

--- TodoList/TodoList/TableViewCell/TodoListCell.swift
+++ TodoList/TodoList/TableViewCell/TodoListCell.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class TodoListCell: UITableViewCell {
-
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbUser: UILabel!
+    @IBOutlet weak var lbDate: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    
+    @IBAction func btnCheckBox(_ sender: UIButton) {
+        sender.isSelected.toggle()
+    }
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
