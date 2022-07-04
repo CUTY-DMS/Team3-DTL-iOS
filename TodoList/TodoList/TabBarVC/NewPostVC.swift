83a790408dc8931653e86e01302bcf438e0487a9
InhyeKang <kanginhae0826@gmail.com>
2022-07-04 22:56:35 +0900

    🌧 :: 상세페이지 기능 수정

---
+++ TodoList/TodoList/TabBarVC/NewPostVC.swift
@@ -0,0 +1,29 @@
+//
+//  NewPostVC.swift
+//  TodoList
+//
+//  Created by 강인혜 on 2022/07/04.
+//
+
+import UIKit
+
+class NewPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
