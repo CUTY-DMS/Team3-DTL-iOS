6462c6d6a19cf5a237bb0aef2563311443b065fc
InhyeKang <kanginhae0826@gmail.com>
2022-07-06 23:50:59 +0900

    🌱 :: 키체인, 탭바, 토큰모델 파일 생성

---
+++ TodoList/TodoList/KeyChain/KeyChain.swift
@@ -0,0 +1,8 @@
+//
+//  KeyChain.swift
+//  TodoList
+//
+//  Created by 강인혜 on 2022/07/06.
+//
+
+import Foundation
---
+++ TodoList/TodoList/Token/TokenModel.swift
@@ -0,0 +1,8 @@
+//
+//  TokenModel.swift
+//  TodoList
+//
+//  Created by 강인혜 on 2022/07/06.
+//
+
+import Foundation
