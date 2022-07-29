54df3f994a2352432f1928e142da59d91afb43a5
InhyeKang <kanginhae0826@gmail.com>
2022-07-29 12:39:11 +0900

    🌱 :: 좋아요 모델파일, 할 일 목록 셀 파일 생성

---
+++ TodoList/TodoList/PostModel/LikeModel.swift
@@ -0,0 +1,13 @@
+//
+//  LikeModel.swift
+//  TodoList
+//
+//  Created by 강인혜 on 2022/07/28.
+//
+
+import Foundation
+
+struct LikeModel: Codable {
+    var like_count: Int
+    var liked: Bool
+}
--- TodoList/TodoList/TableViewCell/TodoListCell.swift
+++ TodoList/TodoList/TableViewCell/TodoListCell.swift
@@ -19,6 +19,7 @@ class TodoListCell: UITableViewCell {
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
 
 }
